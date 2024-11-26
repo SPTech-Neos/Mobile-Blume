89d9f573dcedd8632f604716319e50079f5f4bd0
KevinR-Silva <kevin.rsilva07@gmail.com>
2024-11-25 21:47:51 -0300

    feat/establishment profile (não finalizado)

--- app/src/main/java/com/example/blume_mobile/ui/screens/EstablishmentDetaisl.kt
+++ app/src/main/java/com/example/blume_mobile/ui/screens/EstablishmentDetaisl.kt
@@ -1,13 +1,107 @@
 package com.example.blume_mobile.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.blume_mobile.R
 import com.example.blume_mobile.models.establishment.Establishment
+import com.example.blume_mobile.ui.components.titles.TitleBlume
 import com.example.blume_mobile.ui.theme.Gray700
+import com.example.blume_mobile.ui.theme.Violet500
 
 @Composable
-fun EstablishmentDetails(establishment: Establishment){
-    Text(establishment.name, fontSize = 20.sp, color = Gray700)
+fun EstablishmentDetails(establishment: Establishment) {
+    Column(
+        Modifier
+            .fillMaxSize(),
+        Arrangement.Center,
+        Alignment.CenterHorizontally
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize(0.92f)
+                .padding(top = 6.dp),
 
+        ) {
+            Row(
+                Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(38.dp)
+                        .clip(CircleShape)
+                        .background(Violet500), contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        modifier = Modifier,
+                        painter = painterResource(
+                            id = R.drawable.arrow
+                        ),
+                        contentDescription = "Retornar página"
+                    )
+                }
+                Spacer(modifier = Modifier.width(77.dp))
+                TitleBlume()
+            }
+
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+                    .height(100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Row(
+                    Modifier
+                        .fillMaxWidth(0.9f)
+                        .fillMaxHeight()
+                ) {
+                    if (!establishment.imgUrl.isNullOrEmpty() && establishment.imgUrl.length > 15) {
+                        AsyncImage(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(CircleShape),
+                            model = establishment.imgUrl,
+                            contentDescription = "Imagem de perfil do estabelecimento",
+                            placeholder = painterResource(R.drawable.photo_placeholder),
+                            contentScale = ContentScale.Crop,
+
+                        )
+                    } else {
+                        Image(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(CircleShape),
+                            painter = painterResource(id = R.drawable.photo_placeholder),
+                            contentDescription = "Imagem padrão quando não há imagem"
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
